118b3b25a589c796bfb2ff07a91aba0da079a638
c8in4 <106409156+c8in4@users.noreply.github.com>
2022-06-29 19:51:51 +0200

    go

--- Stoppuhr/script.js
+++ Stoppuhr/script.js
@@ -1,17 +1,47 @@
+let running;
 let hours = 0;
-hours = ("0" + hours).slice(-2);
 let minutes = 0;
-minutes = ("0" + minutes).slice(-2);
 let seconds = 0;
-seconds = ("0" + seconds).slice(-2);
 let milliseconds = 0;
-milliseconds = ("0" + milliseconds).slice(-2);
 
-let running = false;
+function count() {
+    milliseconds++;
+
+    if (milliseconds > 99) {
+        milliseconds = 0;
+        seconds++;
+    }
+    if (seconds > 59) {
+        seconds = 0;
+        minutes++;
+    }
+    if (minutes > 59) {
+        minutes = 0;
+        hours++;
+    }
+
+    milliseconds = ("0" + milliseconds).slice(-2);
+    seconds = ("0" + seconds).slice(-2);
+    minutes = ("0" + minutes).slice(-2);
+    hours = ("0" + hours).slice(-2);
+
+    time.innerHTML = hours + "h : " + minutes + "min : " + seconds + "s : " + milliseconds;
+}
 
 function start() {
     if (!running) {
-        let text = hours + " : " + minutes + " : " + seconds + " : " + milliseconds;
-        time.innerHTML = text;
+        running = true;
+        running = setInterval(count, 10);
+    }
+}
+
+function stop() {
+    clearInterval(running);
+    running = false;
+}
+
+function reset() {
+    if (!running) {
+        time.innerHTML = "00h : 00min : 00s : 00";
     }
 }
